73a475b0b8c7a187b46afc216fbba9dfe48ef8c2
yt-li <yitong.li15@imperial.ac.uk>
2021-06-06 23:16:37 +0100

    For paper

--- UserMain.m
+++ UserMain.m
@@ -1,17 +1,5 @@
 % To users:
 % Please use this file to run toolbox.
-
-%% Tips
-%
-% Please ensure that the toolbox is installed first, by running
-% "InstallSimplexPS.m" once.
-%
-% The toolbox defaultly saves the results into Workspace, prints the key
-% results in Command Window, and plots key figures.
-%
-% For changing default user data, please use "UserData.xlsx". More examples
-% can be found in "Examples" folder.
-
 %% Clear matlab
 clear all;  % Clear matlab workspace
 clc;        % Clear matlab command window
