b2d5faa018dbc2d4f97fadd494739a6bc0c95642
yt-li <yitong.li15@imperial.ac.uk>
2022-01-26 20:18:29 +0000

    For testing two-inverter case

---
+++ TwoInverterCase_GflGfm/GflGfm_Para.m
@@ -0,0 +1,51 @@
+% This function generates the paramters at workspace for simulink model.
+
+% Author(s): Yitong Li
+
+%%
+clear all
+close all
+clc
+
+%%
+t0 = 2;
+dt = 1;
+
+%% Fundamental parameters
+Fs = 50;             % (kHz)
+Fs = Fs*1e3;
+Ts = 1/Fs;
+
+%% Base values
+Wbase = 2*pi*50;    % (rad/s)
+Vbase = 1;
+Sbase = 1;
+Ibase = Sbase/Vbase;
+Zbase = Vbase/Ibase;
+Ybase = 1/Zbase;
+
+%% AC filter parameters
+Lf = 0.05;
+Cf = 0.02;
+Lc = 1e-9;
+
+%% Grid-following inverter
+% PLL
+w_pll = 15*2*pi;    % (rad/s)
+w_tau = 15*2*pi;    % (rad/s)
+
+% Current loop
+w_i_GFL = 250 *2*pi;    % (rad/s)
+
+%% Grid-forming inverter
+% Droop
+m = 5/100;              % (pu)
+w_droop = 15*2*pi;
+
+% Current loop
+w_i_GFM = 500*2*pi;
+
+% Voltage loop
+w_v_GFM = 250 *2*pi;
+Scale_ki_v = 20;
+
