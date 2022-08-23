d6ae30305eced6e25a4aa83445a29b898e419830
Yitong Li <yitong.li15@imperial.ac.uk>
2022-08-23 15:50:42 +0100

    2022 aug18 descriptor state space (#23)
    * Add state participation analysis
    
    Add state participation analysis in Main.m.
    
    Improve variable name style and change some variable names in Main.m.
    
    Debug print in Excel2Json.m.
    
    Add comments in dss2ss.m.
    
    Add SsMinReal.m based on Kalman Decomposition.
    
    Add ObjDss2Ss.m to achieve dss2ss with tracking states and updating state strings.
    
    * Debug, and Update plot and participation analysis
    
    Debug D matrix calculation when A22 is invertable in dss2ss.m
    
    Move default plot in main.m to functions.
    
    Move participation analysis to ModalAnalysisStateSpace.m
    
    Update participation analysis: speed up, add pie chart.

---
+++ +SimplusGT/+Modal/ModalAnalysisStateSpace.m
@@ -0,0 +1,59 @@
+% Author(s): Yitong Li
+
+function ModalAnalysisStateSpace(ObjGsysSs,ModeIndex,FigN)
+    
+    % Get the model and string    
+    [~,GsysSs] = ObjGsysSs.GetSS(ObjGsysSs); 
+    [GsysSsStateStr,~,~] = ObjGsysSs.GetString(ObjGsysSs);
+
+    % Calculate the eigenvalue and eigenvector
+    [PhiMat,EigMat] = eig(GsysSs.A);
+    PsiMat = PhiMat^(-1);
+    EigVec = diag(EigMat);
+    EigVecHz = EigVec/2/pi;
+    
+    % Initialize the participation factor matrix
+    PfMat = zeros(length(GsysSs.A),0);
+    for i = 1:length(EigVecHz)
+        PfMatLambdai = PhiMat(:,i)*PsiMat(i,:);
+        PfMatLambdai = diag(PfMatLambdai);
+        PfMat = [PfMat,PfMatLambdai];
+    end
+    fprintf('Participation factor matrix: PfMat(k,i) \n')
+    fprintf('where i -> ith eigenvalue, k -> kth state or a_kk in state matrix A. \n')
+    
+    for i =1:length(ModeIndex)
+        TargetMode(i) = EigVecHz(ModeIndex(i));
+        PfVec = PfMat(:,ModeIndex(i));
+        PfVec = abs(PfVec);
+        PfVecSum = sum(PfVec);
+        for PfMaxNum = 1:length(PfVec)
+            [PfMaxValue,PfMaxIndex] = maxk(PfVec,PfMaxNum);
+            PfRestIndex = setdiff([1:length(PfVec)],PfMaxIndex);
+            PfRestValue = sum(PfVec(PfRestIndex));
+            PfAllValue = [PfMaxValue;PfRestValue];
+            
+            % The rest of the states that participates less than 10% in total.
+            if PfRestValue/PfVecSum<=0.1 
+                break;
+            end
+        end
+        fprintf('\n')
+        fprintf(['The ' num2str(ModeIndex(i)) 'th eigenvalue (' num2str(TargetMode(i)) ' Hz) is investigated...\n'])
+        fprintf(['Top ' num2str(PfMaxNum) ' largest participation factor(s): \n'])
+        
+        for k = 1:length(PfMaxIndex)
+            fprintf([num2str(PfMat(PfMaxIndex(k),ModeIndex(i))) ' -> ' GsysSsStateStr{PfMaxIndex(k)} '\n']);
+            PfStateStr{k} = GsysSsStateStr{PfMaxIndex(k)};
+        end
+
+        % Plot pie chart
+        if 1
+            figure(FigN)
+            PfStateStr{k+1} = 'Others';
+            pie(PfAllValue,PfStateStr);
+            title(['Participation of the ' num2str(ModeIndex(i)) 'th Eigenvalue (' num2str(TargetMode(i)) ' Hz)'])
+            FigN = FigN+1;
+        end
+    end
+end
