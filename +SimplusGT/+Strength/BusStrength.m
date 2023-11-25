08431d2fa8003b4cd1eb72ad1fd767419f5034be
yt-li <yitong.li15@imperial.ac.uk>
2023-11-25 16:15:29 +0800

    Add bus strength calculation
    Co-Authored-By: amos0115 <151993452+amos0115@users.noreply.github.com>

---
+++ +SimplusGT/+Strength/BusStrength.m
@@ -0,0 +1,33 @@
+% Calculate the Thevenin impedance seen at each bus of a power system. 
+%
+% Author(s): Jialu Yuan, Yitong Li
+
+function [Ydiag,Ybus] = BusImpedance(ApparatusType,ListLine)
+
+[vbus,ibus,fbus] = SimplusGT.Strength.BusTypeVIF(ApparatusType);
+
+% Calculate Ybus
+Ybus = SimplusGT.PowerFlow.YbusCalc(ListLine); 
+ 
+% Sort Ybus
+Ybus_1 = Ybus([vbus,ibus,fbus],:);
+YbusSort = Ybus_1(:,[vbus,ibus,fbus]);
+
+% Calculate Hbus
+HbusSort = SimplusGT.Strength.Y2H(YbusSort,length(vbus));
+
+% Sort Hbus
+Hbus_1 ([vbus,ibus,fbus],:) = HbusSort(:,:);
+Hbus(:,[vbus,ibus,fbus]) = Hbus_1(:,:);
+
+% Calculate bus impedance
+for i = 1:length(Hbus)
+    if (find(vbus,i))
+        Ydiag(i) = Hbus(i,i);
+    else
+        Ydiag(i) = 1/Hbus(i,i);
+    end
+end
+
+end
+
