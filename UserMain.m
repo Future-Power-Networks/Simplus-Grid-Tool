125d906dac54f0f2b5cc05049c0c7a8c0791e4c3
yt-li <yitong.li15@imperial.ac.uk>
2023-12-15 12:57:58 +0800

    Debug
    The 39-bus system is disabled because the system is small-signal unstable.

--- UserMain.m
+++ UserMain.m
@@ -19,7 +19,6 @@ UserDataName = 'UserData';      % Default 4-bus system
 % UserDataName = 'GfmInverterInfiniteBus';   	% Single grid-forming inverter and infinite bus
 % UserDataName = 'IEEE_14Bus';
 % UserDataName = 'IEEE_30Bus';
-% UserDataName = 'IEEE_39Bus';
 % UserDataName = 'IEEE_57Bus';
 % UserDataName = 'AU14Gen_59Bus';
 % UserDataName = 'NETS_NYPS_68Bus';
