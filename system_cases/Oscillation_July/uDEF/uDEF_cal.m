2f226cdc46e680a961347c89e243c920c7a33b7a
Zhu <yz34218@ic.ac.uk>
2024-07-15 11:01:01 +0100

    update PLL part for DEF, with results updated.
    data is too large to upload to github. They are stored in my onedrive.

--- system_cases/Oscillation_July/uDEF/uDEF_cal.m
+++ system_cases/Oscillation_July/uDEF/uDEF_cal.m
@@ -1,19 +1,21 @@
 %save('Oscillation_July/uDEF/out_14bus_case1.mat','out');
-DEF_case=9;
-Napp=8;
+% DEF_case=2;
+Napp=6;
 lengendstr = [{'A1'},{'A2'},{'A3'},{'A6'},{'A8'},{'A11'},{'A12'},{'A13'}];
-if DEF_case==2
-    load('Oscillation_July/uDEF/out_14bus_case2.mat');
-elseif DEF_case==3
-    load('Oscillation_July/uDEF/out_14bus_case1.mat');
-elseif DEF_case== 9
-    load('Oscillation_July/uDEF/out_7bus_case_udef.mat');
-    Napp=6;
-    lengendstr = [{'A1'},{'A3'},{'A4'},{'A5'},{'A6'},{'A7'}];
-end
+% if DEF_case==2
+%     load('Oscillation_July/uDEF/out_14bus_case2.mat');
+% elseif DEF_case==1
+%     load('Oscillation_July/uDEF/out_14bus_case1.mat');
+% elseif DEF_case== 9
+%     load('Oscillation_July/uDEF/out_7bus_case_udef.mat');
+%     Napp=6;
+%     lengendstr = [{'A1'},{'A3'},{'A4'},{'A5'},{'A6'},{'A7'}];
+% end
 
 %Fs=2.5e4;
 new_rate = 1e3;
+figure(11);
+figure(12);
 for i=1:8
     W=zeros(2,4);
     col=mod(i-1,4)+1;
@@ -22,10 +24,9 @@ for i=1:8
     else
         W(1,col)=1;
     end
+    W
     DL=data_length*Fs;
-    km=Napp; % 8 machines
-    
-    
+    km=Napp; % 8 machines  
     uDEF_val=zeros(new_rate*data_length,km);
     
     for kk=1:km
@@ -43,9 +44,11 @@ for i=1:8
         end
     end
     uDEF_TT = timetable(idq.Time,uDEF_val);
-    %figure;
-    %plot(uDEF_TT.Time, uDEF_TT.Var1);
+    figure(12);
+    subplot(2,4,i);
+    plot(uDEF_TT.Time, uDEF_TT.Var1);
     %legend(lengendstr);
-    figure(10+i);
+    figure(11);
+    subplot(2,4,i);
     bar(uDEF_TT.Var1(end,:));
 end
--- system_cases/Oscillation_July/uDEF/uDEF_parameters.m
+++ system_cases/Oscillation_July/uDEF/uDEF_parameters.m
@@ -1,19 +1,10 @@
-if DEF_case==3 % 1.5 Hz case
-    fc_lpf=0.3; %Hz;
-    k_lpf = 0.707; %damping ratio
-    fpll = 3; % pll bandwidth
-    Tdef_start=70;
-    data_length=30; %save x~s data
-    wc_lpf = 2*pi*fc_lpf;
-    fc_lpf2=0.3;
-    [lpf_A,lpf_B,lpf_C,lpf_D]=tf2ss(1,[1/(2*pi*0.3) 1]);
-elseif DEF_case==2 % 18 Hz case
-    fc_lpf=1; %Hz;
-    k_lpf = 0.707; %damping ratio
-    fpll = 3; % pll bandwidth
-    Tdef_start=10;
-    data_length=10; %save x~s data
-    wc_lpf = 2*pi*fc_lpf;
-    fc_lpf2=1;
-    [lpf_A,lpf_B,lpf_C,lpf_D]=tf2ss(1,[1/(2*pi*fc_lpf2) 1]);
-end
+fc_lpf=0.2; %Hz;
+k_lpf = 0.707; %damping ratio
+fpll = 0.5; % pll bandwidth
+Tdef_start=70;
+data_length=30; %save x~s data
+wc_lpf = 2*pi*fc_lpf;
+fc_lpf2=0.2;
+[lpf_A,lpf_B,lpf_C,lpf_D]=tf2ss([wc_lpf^2],[1, 2*k_lpf*wc_lpf,  wc_lpf^2]);
+
+%f_test=8;
